7154c1eb67213adcdda5961bee170c680c4b4eb2
Érico Andrei <ericof@gmail.com>
2026-01-26 14:30:18 -0300

    Back to development: 2.1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = f"""
 
 setup(
     name="kitconcept.seo",
-    version="2.1.2",
+    version="2.1.3.dev0",
     description="SEO optimizations plugin for Plone",
     long_description=long_description,
     long_description_content_type="text/markdown",
