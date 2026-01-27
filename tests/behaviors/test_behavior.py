8aa0f5d6cbea49818936250bbe0e03e2ba2558ee
Érico Andrei <ericof@gmail.com>
2026-01-27 10:44:09 -0300

    Increate test coverage.

--- tests/behaviors/test_behavior.py
+++ tests/behaviors/test_behavior.py
@@ -1,49 +1,57 @@
-def test_seo_behavior_fields(manager_plone_client):
-    # Enable behavior for pages
-    manager_plone_client.patch(
-        "++api++/@controlpanels/dexterity-types/Document",
-        json={"kitconcept.seo": True},
-    )
+from copy import deepcopy
+from kitconcept.seo import PACKAGE_NAME
 
-    # Check schema
-    schema = manager_plone_client.get("++api++/@types/Document").json()
-    assert schema["fieldsets"][-1] == {
-        "behavior": "plone",
-        "description": "",
-        "fields": [
-            "seo_title",
-            "seo_description",
-            "seo_noindex",
-            "seo_canonical_url",
-            "opengraph_title",
-            "opengraph_description",
-            "opengraph_image",
-        ],
-        "id": "seo",
-        "title": "SEO",
-    }
+import pytest
 
 
-def test_noindex_sets_response_header(manager_plone_client):
-    # Enable behavior for pages
-    manager_plone_client.patch(
-        "++api++/@controlpanels/dexterity-types/Document",
-        json={"kitconcept.seo": True},
-    )
+PAYLOAD = {
+    "seo_title": "Foo Bar",
+    "seo_description": "Lorem ipsum dolor sit amet",
+    "seo_noindex": False,
+    "seo_canonical_url": "https://example.com/canonical-url",
+    "opengraph_title": "Open Graph Title",
+    "opengraph_description": "Open Graph Description",
+    "opengraph_image": None,
+}
 
-    # Add page
-    resp = manager_plone_client.post(
-        "++api++/",
-        json={"@type": "Document", "title": "Test page", "seo_noindex": True},
-    )
-    assert resp.status_code == 201
 
-    # Confirm the page is served with X-Robots-Tag header
-    resp = manager_plone_client.get("/test-page")
-    assert resp.status_code == 200
-    assert resp.headers["X-Robots-Tag"] == "noindex"
+@pytest.fixture
+def payload() -> dict:
+    data = deepcopy(PAYLOAD)
+    return data
 
-    # Confirm views of the page are served with X-Robots-Tag header
-    resp = manager_plone_client.get("/test-page/@@view")
-    assert resp.status_code == 200
-    assert resp.headers["X-Robots-Tag"] == "noindex"
+
+class TestBehaviorContato:
+    name: str = f"{PACKAGE_NAME}"
+
+    @pytest.fixture(autouse=True)
+    def _setup(self, portal_factory, dummy_type_schema, manager_html_request):
+        self.portal = portal_factory(behavior=self.name)
+        self.schema = dummy_type_schema()
+        self.request = manager_html_request
+
+    @pytest.mark.parametrize("key", PAYLOAD.keys())
+    def test_behavior_schema(self, key: str):
+        assert key in self.schema["properties"]
+
+    def test_behavior_data(self, payload: dict, create_dummy_content):
+        response = create_dummy_content(payload)
+        assert response.status_code == 201
+
+    def test_noindex_sets_response_header(self):
+        # Add page
+        resp = self.request.post(
+            "++api++/",
+            json={"@type": "DummyType", "title": "Test page", "seo_noindex": True},
+        )
+        assert resp.status_code == 201
+
+        # Confirm the page is served with X-Robots-Tag header
+        resp = self.request.get("/test-page")
+        assert resp.status_code == 200
+        assert resp.headers["X-Robots-Tag"] == "noindex"
+
+        # Confirm views of the page are served with X-Robots-Tag header
+        resp = self.request.get("/test-page/@@view")
+        assert resp.status_code == 200
+        assert resp.headers["X-Robots-Tag"] == "noindex"
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,6 @@
 from kitconcept.seo.testing import FUNCTIONAL_TESTING
 from kitconcept.seo.testing import INTEGRATION_TESTING
-from plone.app.testing import SITE_OWNER_NAME
-from plone.app.testing import SITE_OWNER_PASSWORD
-from plone.restapi.testing import RelativeSession
 from pytest_plone import fixtures_factory
-
-import pytest
 
 
 pytest_plugins = ["pytest_plone"]
@@ -17,11 +12,3 @@ globals().update(
         (INTEGRATION_TESTING, "integration"),
     ))
 )
-
-
-@pytest.fixture
-def manager_plone_client(functional):
-    portal = functional["portal"]
-    api_session = RelativeSession(f"{portal.absolute_url()}")
-    api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
-    return api_session
